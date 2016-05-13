58f84935fbad9512c041ef2451073ce82b5ecc42
Guilherme Solinscki <guilherme.slnk@gmail.com>
2016-05-13 00:24:49 -0300

    Added actions services

--- app/services/digital_ocean_service.rb
+++ app/services/digital_ocean_service.rb
@@ -15,24 +15,49 @@ class DigitalOceanService
   end
 
   def change_status(droplet_id:, status:)
-    droplet = Droplet.find(droplet_id)
+    droplet = droplet_repository.find_by_id(droplet_id)
     if status == 'active'
       power_on(droplet: droplet)
     else
       shutdown(droplet: droplet)
     end
+    droplet
+  end
+
+  def check_pending_actions
+    action_repository.find_all_pending.map do |action|
+      remote_action = retrieve_action(action: action)
+      action = update_action(action.id, remote_action.status)
+      {
+        droplet: action.droplet,
+        action: action,
+      }
+    end
   end
 
   def power_on(droplet:)
-    droplet_actions.power_on(droplet_id: droplet.external_id)
+    action = droplet_actions.power_on(droplet_id: droplet.external_id)
+    action_repository.create_or_update(droplet_id: droplet.id, data: action)
   end
 
   def shutdown(droplet:)
-    shutdown = droplet_actions.shutdown(droplet_id: droplet.external_id)
+    action = droplet_actions.shutdown(droplet_id: droplet.external_id)
+    action_repository.create_or_update(droplet_id: droplet.id, data: action)
+  end
+
+  def retrieve_action(action:)
+    droplet_actions.find(
+      droplet_id: action.droplet.external_id,
+      id: action.external_id
+    )
   end
 
   def droplet_repository
     @droplet_repository ||= DropletRepository.new
+  end
+
+  def action_repository
+    @action_repository ||= ActionRepository.new
   end
 
   private
@@ -40,6 +65,13 @@ class DigitalOceanService
   def update_status(droplet_id, status)
     droplet_repository.update_status(
       droplet_id: droplet_id,
+      status: status
+    )
+  end
+
+  def update_action(action_id, status)
+    action_repository.update_status(
+      action_id: action_id,
       status: status
     )
   end
