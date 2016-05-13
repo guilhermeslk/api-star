82899f5844c3e4f96dec7e9339285dc3d5719a2b
Guilherme Solinscki <guilherme.slnk@gmail.com>
2016-05-13 00:25:54 -0300

    Retrieving droplets list

--- app/controllers/droplets_controller.rb
+++ app/controllers/droplets_controller.rb
@@ -1,11 +1,20 @@
 class DropletsController < ApplicationController
   protect_from_forgery :except => [:update]
 
+  def index
+    DigitalOceanService.new.fetch_and_save_droplets
+    @droplets = DropletRepository.new.find_all
+
+    render json: {
+      html: render_to_string(partial: 'shared/droplets_list.html.erb', locals: { droplets: @droplets })
+    }
+  end
+
   def update
     @droplet = DigitalOceanService.new.change_status(
       droplet_id: params[:id],
       status: params[:status]
     )
-    render json: @droplet
+    render json: { droplet: @droplet, action: @droplet.actions.last }
   end
 end
